1658a1ba05112ecf9065917894cb68b3d6e9dc33
Emiliano <carlostaiperosero@gmail.com>
2026-01-13 18:28:15 -0500

    Corrección de conexión con base de datos local

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,16 @@
 <?php
+/* Desde aquí voy a editar temporalmente para ver si se soluciona el error
+Más abajo tengo editado para ver si puedo conectar. Este código es para InfinityFree
+
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
     require_once __DIR__ . '/config.local.php';
 } else {
     require_once __DIR__ . '/config.prod.php';
 }
+*/
+
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    return require __DIR__ . '/config.local.php';
+}
+
+return require __DIR__ . '/config.prod.php';
